294e62ea8ef1771c6f29ee412e88a4d23d497c11
Jhon Joya <jhonjoya@gmail.com>
2023-03-09 14:50:54 -0500

    Finalizada conexión con DB

--- SistemaGestion/src/main/java/com/mycompany/sistemagestion/dao/ClienteDao.java
+++ SistemaGestion/src/main/java/com/mycompany/sistemagestion/dao/ClienteDao.java
@@ -4,13 +4,24 @@
  */
 package com.mycompany.sistemagestion.dao;
 
+import com.mycompany.sistemagestion.models.Cliente;
+import com.mysql.jdbc.StringUtils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Jhon
  */
 public class ClienteDao {
-
-        public void conectar() {
+        
+        public Connection conectar() {
                 String baseDeDatos = "java";
                 String usuario = "root";
                 String password = "";
@@ -18,5 +29,108 @@ public class ClienteDao {
                 String puerto = "3306";
                 String driver = "com.mysql.jdbc.Driver";
                 String conexionurl = "jdbc:mysql://" + host + ":" + puerto + "/" + baseDeDatos + "?useSSL=false";
+                
+                Connection conexion = null;
+                
+                try {
+                        Class.forName(driver);
+                        conexion = DriverManager.getConnection(conexionurl, usuario, password);
+
+                } catch (Exception ex) {
+                        Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+
+                }
+                return conexion;
         }
+
+        public void agregar(Cliente cliente) {
+                
+                try {
+                        Connection conexion = conectar();
+                        String sql = "INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `telefono`, `email`) VALUES (NULL, '"
+                                + cliente.getNombre() + "', '"
+                                + cliente.getApellido() + "', '"
+                                + cliente.getTelefono()+ "', '"
+                                + cliente.getEmail() + "'); ";
+                        Statement statement = conexion.createStatement();
+                        statement.execute(sql);
+
+                } catch (Exception ex) {
+                        Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+
+                }
+        }
+        
+         public List<Cliente> listar() {
+                
+                List <Cliente> listado = new ArrayList<>();
+                
+                try {
+                        Connection conexion = conectar();             
+                        String sql = "SELECT * FROM `clientes`;";
+                        Statement statement = conexion.createStatement();
+                        ResultSet resultado = statement.executeQuery(sql);
+                        
+                        while(resultado.next()){
+                                Cliente cliente = new Cliente();
+                                cliente.setId(resultado.getString("id"));
+                                cliente.setNombre(resultado.getString("nombre"));
+                                cliente.setApellido(resultado.getString("apellido"));
+                                cliente.setTelefono(resultado.getString("telefono"));
+                                cliente.setEmail(resultado.getString("email"));
+                                
+                                listado.add(cliente);
+                        }
+
+                } catch (Exception ex) {
+                        Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+
+                }
+                return listado;
+        }
+        
+        public void eliminar(String id) {
+                
+                try {
+                        Connection conexion = conectar();
+                        String sql = "DELETE FROM clientes WHERE `id` = " + id;
+                        Statement statement = conexion.createStatement();
+                        statement.execute(sql);
+
+                } catch (Exception ex) {
+                        Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+
+                }
+        }
+         
+        public void actualizar(Cliente cliente) {
+                
+                try {
+                        Connection conexion = conectar();
+                        String sql ;
+                        sql = "UPDATE `clientes` SET `nombre` = '"+ cliente.getNombre()
+                                + "', `apellido` = '"+ cliente.getApellido()
+                                + "', `telefono` = '"+ cliente.getTelefono()
+                                + "', `email` = '"+ cliente.getEmail()
+                                + "' WHERE `id` = "+ cliente.getId()+";";
+
+                        Statement statement = conexion.createStatement();
+                        statement.execute(sql);
+
+                } catch (Exception ex) {
+                        Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+
+                }
+        }
+
+        public void guardar(Cliente cliente) {
+                
+                if (StringUtils.isEmptyOrWhitespaceOnly(cliente.getId())) {
+                        agregar(cliente);
+                } else {
+                        actualizar(cliente);
+                }
+                
+        }
+        
 }
--- SistemaGestion/src/main/java/com/mycompany/sistemagestion/models/Cliente.java
+++ SistemaGestion/src/main/java/com/mycompany/sistemagestion/models/Cliente.java
@@ -10,11 +10,20 @@ package com.mycompany.sistemagestion.models;
  */
 public class Cliente {
 
+        private String id;
         private String nombre;
         private String apellido;
         private String email;
         private String telefono;
 
+        public String getId(){
+                return id;
+        }
+        
+        public void setId(String id){
+                this.id = id;
+        }
+        
         public String getNombre() {
                 return nombre;
         }
