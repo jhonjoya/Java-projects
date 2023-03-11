c342492dd223b0b99f59f52aa6bebd7e1c03cc54
Jhon Joya <jhonjoya@gmail.com>
2023-03-11 14:35:55 -0500

    use fetch in usuario

--- curso-java/src/main/java/com/cursojava/cursojava/controllers/UsuarioController.java
+++ curso-java/src/main/java/com/cursojava/cursojava/controllers/UsuarioController.java
@@ -1,14 +1,58 @@
 package com.cursojava.cursojava.controllers;
 
+import com.cursojava.cursojava.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario123")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario34")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario45")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+
+        return usuario;
     }
 
 }
